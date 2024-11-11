cb1c2686ddba93ce69f2cc781e12a2eaf6a3039e
FolLowCND <l.folcher426@gmail.com>
2024-11-11 09:04:39 -0500

    ajout du dbcontext

--- ProjetV3R_Employe/ProjetV3R_Employe/Program.cs
+++ ProjetV3R_Employe/ProjetV3R_Employe/Program.cs
@@ -3,6 +3,13 @@ using Microsoft.AspNetCore.Components.Web;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Configuration de la base de donnees avec MySQL
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
+    new MySqlServerVersion(new Version(8, 0, 18))));
+
+
+
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
