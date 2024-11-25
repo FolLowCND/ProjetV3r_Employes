17edd222ebd9aed186b1b1a52f7e77712cc9a358
FolLowCND <l.folcher426@gmail.com>
2024-11-24 23:16:14 -0500

    ca me semble compliquer a mettre en place sa race pour une 2eme BD

---
+++ ProjetV3R_Employe/ProjetV3R_Employe/Data/Models/ProjetV3R/ApplicationDbContextProjet.cs
@@ -0,0 +1,15 @@
+//using Microsoft.EntityFrameworkCore;
+
+//namespace ApplicationDbContextProjet.Data
+//{
+//    public class ApplicationDbContext2 : DbContext
+//    {
+//        public ApplicationDbContext2(DbContextOptions<ApplicationDbContext2> options)
+//            : base(options)
+//        {
+//        }
+
+//        // Définissez vos DbSet ici, par exemple :
+//        public DbSet<Fournisseur> Fournisseurs { get; set; }
+//    }
+//}
